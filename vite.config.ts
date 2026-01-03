d1d87c907e0af4e6f3aaddddd2fa7f502ffb961b
tqhoangg08 <tqhoangg2@gmail.com>
2026-01-03 14:06:32 +0700

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig(({ mode }) => {
       target: "esnext",
     },
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY)
       },
       resolve: {
         alias: {
