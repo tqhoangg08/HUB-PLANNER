cb4cd29482893484ed5d95471c48c3d5682f8683
tqhoangg08 <tqhoangg2@gmail.com>
2026-01-16 11:24:33 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
   },
   optimizeDeps: {
+    // 1. Dòng này giúp bỏ qua việc xử lý file PDF gây lỗi
+    exclude: ['pdfjs-dist'], 
+    
+    // 2. Vẫn giữ cấu hình hỗ trợ code mới
     esbuildOptions: {
-      target: 'esnext',
+      target: "esnext",
       supported: { 
         'top-level-await': true 
       },
     },
   },
   build: {
-    target: "esnext", 
+    target: "esnext",
   },
 })
