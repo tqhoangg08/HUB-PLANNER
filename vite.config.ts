d8ffc55679bee71d6ca31c23475d5936658099d1
tqhoangg08 <tqhoangg2@gmail.com>
2026-01-16 11:20:12 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Giữ cố định cổng 3000 cho khớp với Supabase
-  },
-  build: {
-    target: "esnext", // QUAN TRỌNG: Dòng này sửa lỗi màn hình đỏ
-  },
-  esbuild: {
-    target: "esnext", // QUAN TRỌNG: Cho phép chạy code mới nhất
+    port: 3000,
   },
   optimizeDeps: {
     esbuildOptions: {
-      target: "esnext",
+      target: 'esnext',
+      supported: { 
+        'top-level-await': true 
+      },
     },
+  },
+  build: {
+    target: "esnext", 
   },
 })
