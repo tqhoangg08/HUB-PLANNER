d5c2bdb173ead79d8899b8d4e768a90959dee432
tqhoangg08 <tqhoangg2@gmail.com>
2026-01-25 10:46:21 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     const genAI = new GoogleGenerativeAI(apiKey);
 
     const model = genAI.getGenerativeModel({ 
-        model: "gemini-2.5-flash-lite", 
+        model: "gemini-2.0-flash", 
         generationConfig: {
             responseMimeType: "application/json"
         }
@@ -48,4 +48,5 @@ export default async function handler(req, res) {
     });
   }
 }
+
 
