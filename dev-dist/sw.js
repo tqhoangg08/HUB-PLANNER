8195b5feea2ffaddae69066f0be72e393ce78e8c
tqhoangg08 <tqhoangg2@gmail.com>
2026-04-30 18:41:33 +0700

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bcr5vuf5ufk"
+    "revision": "0.kffcdljm2n"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
